1ae611edc4560fb9a3b8b6be90eddd73c7f85d50
Naveenkotapati <113355206+Naveenkotapati@users.noreply.github.com>
2022-09-13 20:21:50 +0530

    Create factorial using recursion.c

---
+++ factorial using recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int factorial(int n)
+{
+	if(n==0)
+	return 1;
+	else 
+	return(n*factorial(n-1));
+}
+int main()
+{
+	int n;
+	long fact;
+	printf("Enter number:");
+	scanf("%d",&n);
+    fact=factorial(n);
+	printf("factorial=%d",fact);
+}
